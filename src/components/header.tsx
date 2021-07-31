c2f4d32629fccf991d52b4cba3d4fe87aabb8b49
Mart Rudolph Macion <martrudolphmacion@Marts-MacBook-Pro.local>
2021-07-31 17:27:58 +0800

    :construction: working on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import './header.scss';
 import {profileDetails} from '../const/details';
+import logo from '../assets/images/logo.svg'
 
 export const Header = () => {
     return <div className="header-container">
-        <div>
+        <div className="header-details">
             <div>
                 <h2>{profileDetails.firstName}</h2>
                 <h2>{profileDetails.lastName}</h2>
@@ -12,6 +13,8 @@ export const Header = () => {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
             </div>
         </div>
-        <div></div>
+        <div className="header-img-container">
+            <img src={logo} alt=""/>
+        </div>
     </div>
 }
