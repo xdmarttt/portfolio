908ee27ba2e9c82c5c781f72fc7869b279e5210d
Mart Rudolph Macion <martrudolphmacion@Marts-MacBook-Pro.local>
2021-07-31 17:12:27 +0800

    :art: add header label

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,13 @@ import {profileDetails} from '../const/details';
 export const Header = () => {
     return <div className="header-container">
         <div>
-            <h2>{profileDetails.firstName}</h2>
-            <h2>{profileDetails.lastName}</h2>
+            <div>
+                <h2>{profileDetails.firstName}</h2>
+                <h2>{profileDetails.lastName}</h2>
+            </div>
+            <div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
         </div>
         <div></div>
     </div>
