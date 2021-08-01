efbd4e5d11ef189f30cb70c8e59ae7708810ff5c
skyMartMacion <martsonic901@gmail.com>
2021-08-01 08:13:44 +0800

    :truck: move const

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,28 +1,11 @@
 import './nav.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBriefcase, faPhone} from '@fortawesome/free-solid-svg-icons';
-import {faAddressCard} from "@fortawesome/free-regular-svg-icons";
-
-
-const navMenu = [
-    {
-        label: 'About',
-        icon: faAddressCard
-    },
-    {
-        label: 'Works',
-        icon: faBriefcase
-    },
-    {
-        label: 'Contact',
-        icon: faPhone
-    }
-]
+import { navMenu } from '../const/nav';
 
 export const Nav = () => {
     return <div className="nav-container">
         <div className="nav-logo">
-            <h4>mrtmcns</h4>
+            <h4>mrtmcn</h4>
         </div>
         <div className="nav-menu">
             <ul>
